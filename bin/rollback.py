110847f225b2568d6a8b0161bc8fcfd210f9337f
root <root@localhost.localdomain>
2011-12-21 02:05:44 +0800

    bin发布

--- bin/rollback.py
+++ bin/rollback.py
@@ -33,46 +33,10 @@ class Rollback:
 		'''回滚该版本'''
 		if len(self.lines) > 0:
 			for line in self.lines:
-				if line[0] != 'A':
-					pass
-
-	def backup(self):
-		'''发布前备份'''
-		if len(self.lines) > 0:
-			for line in self.lines:
-				if line[0] != 'A':
-					lineDir = line[1][0:line[1].rindex('/')]
-					if line[0] == 'D' and os.path.isdir('%s/%s' % (self.wwwDir, line[1])):
-						os.system('mkdir -p %s/%s' % (self.curBackup, line[1]))
-
-					if os.path.isdir('%s/%s' % (self.curBackup, lineDir)) is False:
-						os.system('mkdir -p %s/%s' % (self.curBackup, lineDir))
-
-					if os.path.isfile('%s/%s' % (self.wwwDir, line[1])):
-						os.system('cp %s/%s %s/%s' % (self.wwwDir, line[1], self.curBackup, line[1]))
-
-			return True
-		else:
-			return False
-
-	def release(self):
-		'''开始发布'''
-		if len(self.lines) > 0:
-			for line in self.lines:
-				if line[0] != 'D':
-					if line[0] == 'A' and os.path.isdir('%s/%s' % (self.wwwDir, line[1])) is False:
-						os.system('mkdir -p %s/%s' % (self.wwwDir, line[1]))
-
-					lineDir = line[1][0:line[1].rindex('/')]
-					if os.path.isdir('%s/%s' % (self.wwwDir, lineDir)) is False:
-						os.system('mkdir -p %s/%s' % (self.wwwDir, lineDir))
-
-					if os.path.isfile('%s/%s' % (self.curSource, line[1])):
-						os.system('cp %s/%s %s/%s' % (self.curSource, line[1], self.wwwDir, line[1]))
+				if line[0] == 'A':
+					os.system('rm -rf %s/%s' % (self.wwwDir, line[1]))
 				else:
-					if os.path.isdir('%s/%s' % (self.wwwDir, line[1]) or os.path.isfile('%s/%s' % (self.wwwDir, line[1]) or os.path.isfile())):
-						os.system('rm -rf %s/%s' % (self.wwwDir, line[1]))
-
+					os.system('cp -rf %s/%s %s/%s' % (self.curBackup, line[1], self.wwwDir, line[1]))
 			return True
 		else:
 			return False
@@ -85,23 +49,17 @@ class Rollback:
 		return True
 
 def usage():
-	print 'Usage:python release.py 2.0.15.r8888 /data/server/a0b923820dcc509a /data/wwwV2'
+	print 'Usage:python rollback.py 2.0.15.r8888 /data/server/a0b923820dcc509a /data/wwwV2'
 
 if __name__ == '__main__':
 	if len(sys.argv) < 4:
 		usage()
 		sys.exit(2)
 
-	ser = Server()
-
-	print '发布前备份文件...'
-	if ser.backup() == False:
+	rb = RollBack()
+	if rb.rollBack() == False:
 		print 'log文件不存在，备份失败'
 		sys.exit(2)
 
-	print '开始发布...'
-	if ser.release() == False:
-		print ''
-		sys.exit(2)
-	print '发布清理...'
-	ser.clear()
+	print '版本%s回滚成功' % sys.argv[1]
+	rb.clear()
