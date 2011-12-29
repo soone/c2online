80bdf0818ff5d687a4520af0b2a810d33717df10
soone <fengyue15@163.com>
2011-12-29 19:38:47 +0800

    a

--- web/modules/vpndialup.py
+++ web/modules/vpndialup.py
@@ -10,7 +10,7 @@ class VpnDialUp:
 	def pptpStart(self, host, user, pw):
 		'''pptp拨号'''
 		pptpName = hashlib.new('md5', host).hexdigest()[8 : -8]
-		rs = os.popen('pptpsetup --create %s --server %s --username %s --password %s --encrypt --start' % (pptpName, host, user, pw))
+		rs = os.popen('sudo pptpsetup --create %s --server %s --username %s --password %s --encrypt --start' % (pptpName, host, user, pw))
 		print rs.readlines()
 
 	def pptpStop(self, host):
